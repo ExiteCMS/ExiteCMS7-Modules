28449ea9be2a4ac5d4830695d063f33a6a055ec5
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2007-09-21 09:02:43 +0000

    Added the PhotoGallery module

---
+++ gallery/php-files/modules/gallery/index.php
@@ -0,0 +1,175 @@
+<?php
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2007 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id: index.php 15919 2007-03-05 15:14:55Z jenst $
+ */
+?>
+<?php
+// Hack prevention.
+
+/*---------------------------------------------------+
+| PLi-Fusion Content Management System               |
++----------------------------------------------------+
+| Integrated by WanWizard (wanwizard@gmail.com)      |
+| http://www.pli-images.org/pli-fusion               |
++----------------------------------------------------*/
+require_once dirname(__FILE__)."/../../includes/core_functions.php";
+require_once PATH_ROOT."/includes/theme_functions.php";
+
+global $GALLERY_EMBEDDED_INSIDE;
+global $GALLERY_EMBEDDED_INSIDE_TYPE;
+global $GALLERY_MODULENAME;
+global $MOS_GALLERY_PARAMS;
+
+// Mambo / Joomla calls index.php directly for popups - we need to make
+// sure that the option var has been extracted into the environment
+// otherwise it just won't work.
+$option = isset($_REQUEST['option']) ? $_REQUEST['option'] : null;
+$op = isset($_REQUEST['op']) ? $_REQUEST['op'] : null;
+$mop = isset($_REQUEST['mop']) ? $_REQUEST['mop'] : null;
+$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+$include = isset($_REQUEST['include']) ? $_REQUEST['include'] : null;
+
+// tell Gallery we're embedding in ExiteCMS
+$GALLERY_EMBEDDED_INSIDE = 'ExiteCMS';
+$GALLERY_EMBEDDED_INSIDE_TYPE = 'ExiteCMS';
+
+// default Gallery entry point
+if (empty($include)) {
+	$include = "albums.php";
+}
+
+// start capturing Gallery output
+ob_start();
+
+/*
+ * As a security precaution, only allow one of the following files to be included.
+ * If you want Gallery to allow you to include other files (such as the random photo block)
+ * then you need to add the name of the file including any relevant path components to this
+ * array.
+ */
+$safe_to_include =
+	 array(
+	       "add_comment.php",
+	       "add_photos.php",
+	       "add_photos_frame.php",
+	       "admin-page.php",
+	       "administer_startpage.php",
+	       "album_permissions.php",
+	       "albums.php",
+	       "block-random.php",
+	       "captionator.php",
+	       "copy_photo.php",
+	       "create_user.php",
+	       "delete_album.php",
+	       "delete_photo.php",
+	       "delete_user.php",
+	       "do_command.php",
+	       "download.php",
+	       "ecard_form.php",
+	       "edit_appearance.php",
+	       "edit_caption.php",
+	       "edit_field.php",
+	       "edit_thumb.php",
+	       "edit_watermark.php",
+	       "extra_fields.php",
+	       "gallery_remote.php",
+	       "gallery_remote2.php",
+	       "help/imagemap.php",
+	       "help/metadataOnUpload.php",
+	       "highlight_photo.php",
+	       "imagemap.php",
+	       "lib/colorpicker.php",
+	       "login.php",
+	       "manage_users.php",
+	       "modify_user.php",
+	       "move_album.php",
+	       "move_photo.php",
+	       "multi_create_user.php",
+	       "photo_owner.php",
+	       "poll_properties.php",
+	       "poll_results.php",
+	       "progress_uploading.php",
+	       "publish_xp.php",
+	       "publish_xp_docs.php",
+	       "rearrange.php",
+	       "rebuild_capture_dates.php",
+	       "register.php",
+	       "rename_album.php",
+	       "reset_votes.php",
+	       "resize_photo.php",
+	       "rotate_photo.php",
+	       "rss.php",
+	       "save_photos.php",
+	       "search.php",
+	       "slideshow.php",
+	       "slideshow_high.php",
+	       "slideshow_low.php",
+	       "sort_album.php",
+	       "stats-wizard.php",
+	       "stamp_preview.php",
+	       "stats.php",
+	       "tools/find_orphans.php",
+	       "tools/despam-comments.php",
+	       "tools/validate_albums.php",
+	       "upgrade_album.php",
+	       "upgrade_users.php",
+	       "user_preferences.php",
+	       "view_album.php",
+	       "view_comments.php",
+	       "view_photo.php",
+	       "view_photo_properties.php",
+	       "watermark_album.php",
+	       );
+
+if (!in_array($include, $safe_to_include)) {
+    $include = htmlentities($include);
+    print sprintf(_("Security error!  The file you tried to include is not on the <b>approved file list</b>.  To include this file you must edit %s's index.php and add <b>%s</b> to the <i>\$safe_to_include</i> array"),
+		    'Gallery', $include);
+} else {
+	include(dirname(__FILE__) . "/$include");
+}
+
+// get the captured content
+$content =  ob_get_contents();
+
+// make sure the content is not cached
+// header("Cache-Control: no-cache");
+
+// clear the output buffer
+ob_end_clean();
+
+/** 
+ * Output the page.
+ */
+if (substr($_SERVER["QUERY_STRING"], -4) == ".xml") {
+
+	echo $content;
+
+} else {
+
+	// create the panel
+	$variables['html'] = $content;
+	$template_panels[] = array('type' => 'body', 'title' => 'Gallery', 'name' => 'gallery', 'template' => '_custom_html.tpl');
+	$template_variables['gallery'] = $variables;
+
+	// and call the template engine to generate the page
+	require_once PATH_THEME."/theme.php";
+}
+?>
---
+++ gallery/php-files/modules/gallery/init.php
@@ -0,0 +1,285 @@
+<?php
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2007 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id: init.php 15632 2007-01-02 06:01:08Z jenst $
+*/
+?>
+<?php
+
+// Hack Prevention
+$sensitiveList = array('gallery', 'GALLERY_EMBEDDED_INSIDE', 'GALLERY_EMBEDDED_INSIDE_TYPE', 'GLOBALS');
+foreach ($sensitiveList as $sensitive) {
+    if (!empty($_REQUEST[$sensitive])) {
+        echo "Security violation! Override attempt.\n";
+        exit;
+    }
+}
+
+/*
+* Turn down the error reporting to just critical errors for now.
+* In v1.2, we know that we'll have lots and lots of warnings if
+* error reporting is turned all the way up.  We'll fix this in v2.0
+*/
+error_reporting(E_ALL & ~E_NOTICE);
+
+/*
+*  Seed the randomization pool once, instead of doing it every place
+*  that we use rand() or mt_rand()
+*/
+mt_srand((double) microtime() * 1000000);
+
+global $gallery;
+require(dirname(__FILE__) . "/Version.php");
+require(dirname(__FILE__) . "/util.php");
+
+/* Load bootstrap code */
+if (getOS() == OS_WINDOWS) {
+    include_once(dirname(__FILE__) . "/platform/fs_win32.php");
+} else {
+    include_once(dirname(__FILE__) . "/platform/fs_unix.php");
+}
+
+if (fs_file_exists(dirname(__FILE__) . "/config.php")) {
+    include_once(dirname(__FILE__) . "/config.php");
+
+    /* Here we set a default execution time limit for the entire Gallery script
+     * the value is defined by the user during setup, so we want it inside the
+     * 'if config.php' block.  If the user increases from the default, this will cover
+     * potential execution issues on slow systems, or installs with gigantic galleries.
+     * By calling set_time_limit() again further in the script (in locations we know can
+     * take a long time) we reset the counter to 0 so that a longer execution can occur.
+    */
+    set_time_limit($gallery->app->timeLimit);
+}
+
+/*
+ *  We TRY to make sure that register_globals is disabled.  If the user has not disabled
+ *  register_globals in their php.ini, we try to emulate its functionality by unsetting all
+ *  variables from $_REQUEST.  Some *Nuke systems apparently do not function well with this
+ *  emulation, so we have given users a method to opt-out.
+ *
+ *  WE DO NOT OFFICIALLY SUPPORT THE USE OF skipRegisterGlobals BECAUSE IT COULD POTENTIALLY
+ *  OPEN Gallery TO SECURITY RISKS!  This is for advanced users only.
+*/
+if (empty($gallery->app->skipRegisterGlobals) || $gallery->app->skipRegisterGlobals != "yes") {
+    $register_globals = @ini_get('register_globals');
+    if (!empty($register_globals) && !eregi("no|off|false", $register_globals)) {
+        foreach (array_keys($_REQUEST) as $key) {
+            unset($$key);
+        }
+    }
+}
+
+// Optional developer hook - location to add useful
+// functions such as code profiling modules
+if (file_exists(dirname(__FILE__) . "/lib/devel.php")) {
+    require_once(dirname(__FILE__) . "/lib/devel.php");
+}
+
+/*
+ * Now we can catch if were are in GeekLog and if yes, include the common lib file.
+ *
+ * If the old example path is still set, remove it.
+*/
+if (!empty($gallery->app->geeklog_dir) && $gallery->app->geeklog_dir == "/path/to/geeklog/public_html") {
+    $gallery->app->geeklog_dir = '';
+}
+
+// Verify that the geeklog_dir isn't overwritten with a remote exploit
+if (!empty($gallery->app->geeklog_dir) && !realpath($gallery->app->geeklog_dir)) {
+    print _("Security violation. Geeklog Dir is invalid.") ."\n";
+    exit;
+}
+elseif (!empty($gallery->app->geeklog_dir)) {
+    $GALLERY_EMBEDDED_INSIDE='GeekLog';
+    $GALLERY_EMBEDDED_INSIDE_TYPE = 'GeekLog';
+
+    if (! defined ("GEEKLOG_DIR")) {
+        define ("GEEKLOG_DIR",$gallery->app->geeklog_dir);
+    }
+
+    require_once(GEEKLOG_DIR . '/lib-common.php');
+}
+
+if (isset($gallery->app->devMode) && $gallery->app->devMode == 'yes') {
+    ini_set("display_errors", "1");
+    error_reporting(E_ALL);
+}
+else {
+    error_reporting(E_ALL & ~E_NOTICE);
+}
+
+/*
+ * Detect if we're running under SSL and adjust the URL accordingly.
+*/
+if(isset($gallery->app)) {
+    if (isset($_SERVER["HTTPS"] ) && stristr($_SERVER["HTTPS"], "on")) {
+        $gallery->app->photoAlbumURL =
+            eregi_replace("^http:", "https:", $gallery->app->photoAlbumURL);
+        $gallery->app->albumDirURL =
+            eregi_replace("^http:", "https:", $gallery->app->albumDirURL);
+    } else {
+        $gallery->app->photoAlbumURL =
+            eregi_replace("^https:", "http:", $gallery->app->photoAlbumURL);
+        $gallery->app->albumDirURL =
+            eregi_replace("^https:", "http:", $gallery->app->albumDirURL);
+    }
+
+    /*
+     * We have a Coral (http://www.scs.cs.nyu.edu/coral/) request coming in, adjust outbound links
+    */
+    if(isset($_SERVER['HTTP_USER_AGENT']) && strstr($_SERVER['HTTP_USER_AGENT'], 'CoralWebPrx')) {
+        if (ereg("^(http://[^:]+):(\d+)(.*)$", $gallery->app->photoAlbumURL)) {
+            $gallery->app->photoAlbumURL = ereg_replace("^(http://[^:]+):(\d+)(.*)$", "\1.\2\3", $galllery->app->photoAlbumURL);
+        }
+
+        $gallery->app->photoAlbumURL = ereg_replace("^(http://[^/]+)(.*)$", '\1.nyud.net:8090\2',$gallery->app->photoAlbumURL);
+        if (ereg("^(http://[^:]+):(\d+)(.*)$", $gallery->app->albumDirURL)) {
+            $gallery->app->albumDirURL = ereg_replace("^(http://[^:]+):(\d+)(.*)$", "\1.\2\3", $galllery->app->albumDirURL);
+        }
+        $gallery->app->albumDirURL = ereg_replace("^(http://[^/]+)(.*)$", '\1.nyud.net:8090\2',$gallery->app->albumDirURL);
+    }
+}
+
+/*
+ * Turn off magic quotes runtime as they interfere with saving and
+ * restoring data from our file-based database files
+*/
+set_magic_quotes_runtime(0);
+
+if (!isset($GALLERY_NO_SESSIONS)) {
+    require(dirname(__FILE__) . "/session.php");
+}
+
+// We need to init the language before we include the files below, as they contain gettext calls.
+initLanguage();
+
+/* Load classes
+ * Note: Some classes and libs are loaded in util.php
+*/
+require(dirname(__FILE__) . "/classes/Album.php");
+require(dirname(__FILE__) . "/classes/Image.php");
+require(dirname(__FILE__) . "/classes/AlbumItem.php");
+require(dirname(__FILE__) . "/classes/AlbumDB.php");
+require(dirname(__FILE__) . "/classes/User.php");
+require(dirname(__FILE__) . "/classes/EverybodyUser.php");
+require(dirname(__FILE__) . "/classes/NobodyUser.php");
+require(dirname(__FILE__) . "/classes/LoggedInUser.php");
+require(dirname(__FILE__) . "/classes/UserDB.php");
+require(dirname(__FILE__) . "/classes/Comment.php");
+
+$gallerySanity = gallerySanityCheck();
+
+/* Make sure that Gallery is set up properly */
+if ($gallerySanity != NULL) {
+    include_once(dirname(__FILE__) . "/includes/errors/$gallerySanity");
+    exit;
+}
+
+if (isset($GALLERY_EMBEDDED_INSIDE)) {
+    /* Okay, we are embedded */
+    switch($GALLERY_EMBEDDED_INSIDE_TYPE) {
+        case 'ExiteCMS':
+            //print_r($GLOBALS['board_config']['version']);
+            include_once(dirname(__FILE__) . "/classes/Database.php");
+            include_once(dirname(__FILE__) . "/classes/database/mysql/Database.php");
+            include_once(dirname(__FILE__) . "/classes/ExiteCMS/UserDB.php");
+            include_once(dirname(__FILE__) . "/classes/ExiteCMS/User.php");
+
+          	$dbhost = $GLOBALS['db_host'];
+        	$dbuser = $GLOBALS['db_user'];
+        	$dbpasswd = $GLOBALS['db_passwd'];
+        	$dbname = $GLOBALS['db_name'];
+
+            $gallery->database{"phpbb"} = new MySQL_Database($dbhost, $dbuser, $dbpasswd, $dbname);
+
+            //	    $gallery->database{"phpbb"}->setTablePrefix($GLOBALS['table_prefix']);
+            $gallery->database{"prefix"} = $GLOBALS['table_prefix'];
+            /* Load our user database (and user object) */
+            $gallery->userDB = new phpbb_UserDB;
+            if (isset($GLOBALS['userdata']) && isset($GLOBALS['userdata']['username'])) {
+                $gallery->session->username = $GLOBALS['userdata']['username'];
+                $gallery->user = $gallery->userDB->getUserByUsername($gallery->session->username);
+            }
+            elseif ($gallery->session->username) {
+                $gallery->user = $gallery->userDB->getUserByUsername($gallery->session->username);
+            }
+
+            break;
+    }
+}
+else {
+    /* Standalone */
+    include_once(dirname(__FILE__) . "/classes/gallery/UserDB.php");
+    include_once(dirname(__FILE__) . "/classes/gallery/User.php");
+
+    /* Load our user database (and user object) */
+    $gallery->userDB = new Gallery_UserDB;
+
+    /* Load their user object with their username as the key */
+    if (isset($gallery->session->username) && !empty($gallery->session->username)) {
+        $gallery->user = $gallery->userDB->getUserByUsername($gallery->session->username);
+    }
+}
+
+/* If there's no specific user, they are the special Everybody user */
+if (!isset($gallery->user) || empty($gallery->user)) {
+    if (empty($gallery->userDB)) {
+        exit("Fatal error: UserDB failed to initialize!");
+    }
+    $gallery->user = $gallery->userDB->getEverybody();
+    $gallery->session->username = "";
+}
+
+/*
+ * 18.12.2006
+ * In previous Versions we did a second language init after the user init.
+ * But i do'nt see a reason a reason. So i commented it out.
+ if (!empty($gallery->user)) {
+     $userlanguage = $gallery->user->getDefaultLanguage();
+ 
+     if($userlanguage != $gallery->language) {
+         initLanguage(true, $userlanguage);
+     }
+ }
+*/
+
+if (!isset($gallery->session->offline)) {
+    $gallery->session->offline = FALSE;
+}
+
+if ($gallery->userDB->versionOutOfDate()) {
+    include_once(dirname(__FILE__) . "/upgrade_users.php");
+    exit;
+}
+
+/* Load the correct album object */
+if (!empty($gallery->session->albumName)) {
+    $gallery->album = new Album;
+    $ret = $gallery->album->load($gallery->session->albumName);
+    if (!$ret) {
+        $gallery->session->albumName = "";
+    } else {
+        if ($gallery->album->versionOutOfDate()) {
+            include_once(dirname(__FILE__) . "/upgrade_album.php");
+            exit;
+        }
+    }
+}
+?>
