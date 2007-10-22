df36e18d856e0bc52f49cbff4dc6e1b92f68e3f5
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2007-10-22 21:49:39 +0000

    Removed PLi specific text from the locale

--- birthday_panel/php-files/modules/birthday_panel/locale/English.php
+++ birthday_panel/php-files/modules/birthday_panel/locale/English.php
@@ -1,10 +1,10 @@
 <?php
 // Infusion titles & description
 $locale['bdp100'] = "Congratulations!";
-$locale['bdp101'] = "This infusion a hppy birthday panel";
+$locale['bdp101'] = "This infusion a happy birthday panel";
 $locale['bdp102'] = "";
 $locale['bdpver'] = "0.0.1";
 //
-$locale['bdp110'] = "The PLi® team wishes you a very happy birthday!";
-$locale['bdp111'] = "The PLi® team wishes you all a very happy birthday!";
+$locale['bdp110'] = "We wish you a very happy birthday!";
+$locale['bdp111'] = "We wish you all a very happy birthday!";
 ?>
