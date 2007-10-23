22d5ea94ff44761a483a080b24e45cbd22738ad9
hverton <hverton@f05d0674-473a-0410-9a19-83e10c34e0e8>
2007-10-23 08:36:42 +0000

    Added a WriteAccess check, and adjust the stub text accordingly

--- wiki/php-files/modules/wiki/actions/stub.php
+++ wiki/php-files/modules/wiki/actions/stub.php
@@ -9,10 +9,15 @@
     * DavePreston (www.davepreston.me.uk)
     */
 	$stub = ">>@@This page is a **WikiStub**@@>>";
-	$stub .= "===Be the first to edit this page!===\n\n\n";
-	$stub .= "If you are a member of this website, you can help to make this Wiki more useful by adding information to it.\n\nJust click on the \"Edit page\" link at the bottom left of the page or double-click on the page text.\n\n";
-	$stub .= "If this is the first time you add information to this Wiki, please make sure you are familiar with the WikiRules and the FormattingRules, that will save us a lot of editing.\n\n";
-	$stub .= "Thank you for your efforts in making this Wiki grow!";
+	if ($this->HasAccess("write")) {
+		$stub .= "===Be the first to edit this page!===\n\n\n";
+		$stub .= "You can help to make this Wiki more useful by adding information to it.\n\nJust click on the \"Edit page\" link at the bottom left of the page or double-click on the page text.\n\n";
+		$stub .= "If this is the first time you add information to this Wiki, please make sure you are familiar with the WikiRules and the FormattingRules. This will create a consistent look-and-feel, and saves us the work of correcting your text!\n\n";
+		$stub .= "Thank you for your efforts in making this Wiki grow!";
+	} else {
+		$stub .= "===No text has been entered for this page!===\n\n\n";
+		$stub .= "There is no text available for the Wiki Name you selected.";
+	}
 	
     echo $this->Format($stub);
 ?>
