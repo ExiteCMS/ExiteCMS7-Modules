948243c59451120d287fb640dbbf725ded023bfb
root <root@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-06-06 17:42:22 +0000

    removed (for the moment) the old wiki code tree, to make vendor branching possible

--- wiki/php-files/modules/wiki/actions/pageviews.php
+++
@@ -1,9 +0,0 @@
-<?
-// Start output of counter
-$thispage=$this->GetPageTag();
-$result = mysql_query("SELECT hits FROM ".$this->config["table_prefix"]."pages WHERE tag='$thispage' AND latest='Y'");
-$row = mysql_fetch_array($result);
-$pagehits = $row['hits'];
-print "$pagehits";
-// End Output of counter
-?>
