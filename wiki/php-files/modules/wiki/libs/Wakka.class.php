01301d38b44a03d091766f33aa7752a72ee2c974
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-10-27 23:00:28 +0000

    Remove pages from the {{WantedPages}} when the wiki tag alias exists for the page

--- wiki/php-files/modules/wiki/libs/Wakka.class.php
+++ wiki/php-files/modules/wiki/libs/Wakka.class.php
@@ -560,7 +560,7 @@ class Wakka
 			return $pages;
 		}
 	}
-	function LoadWantedPages() { return $this->LoadAll("select distinct ".$this->config["table_prefix"]."links.to_tag as tag,count(".$this->config["table_prefix"]."links.from_tag) as count from ".$this->config["table_prefix"]."links left join ".$this->config["table_prefix"]."pages on ".$this->config["table_prefix"]."links.to_tag = ".$this->config["table_prefix"]."pages.tag where ".$this->config["table_prefix"]."pages.tag is NULL group by tag order by count desc"); }
+	function LoadWantedPages() { return $this->LoadAll("select distinct ".$this->config["table_prefix"]."links.to_tag as tag,count(".$this->config["table_prefix"]."links.from_tag) as count from ".$this->config["table_prefix"]."links left join ".$this->config["table_prefix"]."pages on ".$this->config["table_prefix"]."links.to_tag = ".$this->config["table_prefix"]."pages.tag left join ".$this->config["table_prefix"]."aliases on ".$this->config["table_prefix"]."links.to_tag = ".$this->config["table_prefix"]."aliases.from_tag where ".$this->config["table_prefix"]."pages.tag is NULL AND ".$this->config["table_prefix"]."aliases.from_tag is NULL group by tag order by count desc"); }
 	function IsWantedPage($tag)
 	{
 		if ($pages = $this->LoadWantedPages())
