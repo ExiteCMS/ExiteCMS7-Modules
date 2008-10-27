53d5ed4b48e5776395c2cb325308c6d99f049b5f
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-10-27 16:40:21 +0000

    added optional width, height and style parameters to the {{image}} wiki action

--- wiki/php-files/modules/wiki/actions/image.php
+++ wiki/php-files/modules/wiki/actions/image.php
@@ -8,11 +8,13 @@
 		title		- title text displayed when mouse hovers above image
 		class		- a class for the image
 		alt         - an alt text
-
+		width		- optional width of the image
+		height		- optional height of the image
+		style       - optional image style
 */
 
 $title = 'WikiImage';
-$class = $link = '';
+$class = $link = $style = '';
 $alt = 'image';
 
 if (is_array($vars))
@@ -24,6 +26,9 @@ if (is_array($vars))
     	if ($param == 'class') {$class = $this->htmlspecialchars_ent($vars['class']);}
     	if ($param == 'alt') {$alt = $this->htmlspecialchars_ent($vars['alt']);}
     	if ($param == 'link') {$link = $this->htmlspecialchars_ent($vars['link']);}
+    	if ($param == 'width') {$width = $this->htmlspecialchars_ent($vars['width']);}
+    	if ($param == 'height') {$width = $this->htmlspecialchars_ent($vars['height']);}
+    	if ($param == 'style') {$style = $this->htmlspecialchars_ent($vars['style']);}
 	}
 }
 $url = $this->cleanUrl(trim($vars['url']));
@@ -31,8 +36,10 @@ $url = $this->cleanUrl(trim($vars['url']));
 // no path given? Then add the default wiki path
 if (strpos($url, "/") === false) $url = IMAGES."wiki/".$url;
 
-$output = '<img class="'.$class.'" src="'.$url.'" alt="'.$alt.'" title="'.$title.'" />';
-
+$output = '<img class="'.$class.'" src="'.$url.'"';
+if (isset($width)) $output .= ' width="'.$width.'"';
+if (isset($height)) $output .= ' height="'.$height.'"'; 
+$output .= ' style="'.$style.'" alt="'.$alt.'" title="'.$title.'" />';
 
 // link?
 if ($link !== '')
