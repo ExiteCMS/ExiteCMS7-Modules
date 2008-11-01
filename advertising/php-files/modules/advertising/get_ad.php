9fc28784a13ef75a53681f7552d082ce6af6ddc4
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-11-01 14:34:42 +0000

    source code header update

--- advertising/php-files/modules/advertising/get_ad.php
+++ advertising/php-files/modules/advertising/get_ad.php
@@ -1,4 +1,21 @@
 <?php
+/*---------------------------------------------------------------------+
+| ExiteCMS Content Management System                                   |
++----------------------------------------------------------------------+
+| Copyright 2006-2008 Exite BV, The Netherlands                        |
+| for support, please visit http://www.exitecms.org                    |
++----------------------------------------------------------------------+
+| Some code derived from PHP-Fusion, copyright 2002 - 2006 Nick Jones  |
++----------------------------------------------------------------------+
+| Released under the terms & conditions of v2 of the GNU General Public|
+| License. For details refer to the included gpl.txt file or visit     |
+| http://gnu.org                                                       |
++----------------------------------------------------------------------+
+| $Id:: viewpage.php 1935 2008-10-29 23:42:42Z WanWizard              $|
++----------------------------------------------------------------------+
+| Last modified by $Author:: WanWizard                                $|
+| Revision number $Rev:: 1935                                         $|
++---------------------------------------------------------------------*/
 
 // main ad function. This function returns html code (anchor + image) for an advert of the requested location
 
