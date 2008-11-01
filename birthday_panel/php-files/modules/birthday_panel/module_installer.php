7a5d83650ab2652968b8d9044524ba50ac3652b6
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-11-01 14:45:16 +0000

    source code header update

--- birthday_panel/php-files/modules/birthday_panel/module_installer.php
+++ birthday_panel/php-files/modules/birthday_panel/module_installer.php
@@ -1,14 +1,21 @@
 <?php
-/*---------------------------------------------------+
-| ExiteCMS Content Management System                 |
-+----------------------------------------------------+
-| Copyright 2007 Harro "WanWizard" Verton, Exite BV  |
-| for support, please visit http://exitecms.exite.eu |
-+----------------------------------------------------+
-| Released under the terms & conditions of v2 of the |
-| GNU General Public License. For details refer to   |
-| the included gpl.txt file or visit http://gnu.org  |
-+----------------------------------------------------*/
+/*---------------------------------------------------------------------+
+| ExiteCMS Content Management System                                   |
++----------------------------------------------------------------------+
+| Copyright 2006-2008 Exite BV, The Netherlands                        |
+| for support, please visit http://www.exitecms.org                    |
++----------------------------------------------------------------------+
+| Some code derived from PHP-Fusion, copyright 2002 - 2006 Nick Jones  |
++----------------------------------------------------------------------+
+| Released under the terms & conditions of v2 of the GNU General Public|
+| License. For details refer to the included gpl.txt file or visit     |
+| http://gnu.org                                                       |
++----------------------------------------------------------------------+
+| $Id:: viewpage.php 1935 2008-10-29 23:42:42Z WanWizard              $|
++----------------------------------------------------------------------+
+| Last modified by $Author:: WanWizard                                $|
+| Revision number $Rev:: 1935                                         $|
++---------------------------------------------------------------------*/
 if (!checkrights("I") || !defined("iAUTH") || $aid != iAUTH || !defined('INIT_CMS_OK')) fallback(BASEDIR."index.php");
 
 /*---------------------------------------------------+
@@ -16,7 +23,7 @@ if (!checkrights("I") || !defined("iAUTH") || $aid != iAUTH || !defined('INIT_CM
 +----------------------------------------------------*/
 $mod_title = "Birthday side panel";
 $mod_description = "Shows today birthdays";
-$mod_version = "1.1.0";
+$mod_version = "1.1.1";
 $mod_developer = "WanWizard";
 $mod_email = "wanwizard@exitecms.org";
 $mod_weburl = "http://www.exitecms.org/";
