ce879dc91694914211ac6d40a7e9783e3956bdd3
WanWizard <WanWizard@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-11-01 14:45:29 +0000

    source code header update

--- eXtplorer/php-files/modules/eXtplorer/index.php
+++ eXtplorer/php-files/modules/eXtplorer/index.php
@@ -1,10 +1,21 @@
 <?php
-/*---------------------------------------------------+
-| ExiteCMS Content Management System                 |
-+----------------------------------------------------+
-| Integrated by WanWizard (wanwizard@gmail.com)      |
-| http://exitecms.exite.eu                           |
-+----------------------------------------------------*/
+/*---------------------------------------------------------------------+
+| ExiteCMS Content Management System                                   |
++----------------------------------------------------------------------+
+| Copyright 2006-2008 Exite BV, The Netherlands                        |
+| for support, please visit http://www.exitecms.org                    |
++----------------------------------------------------------------------+
+| Integrated by WanWizard (wanwizard@exitecms.org)                     |
++----------------------------------------------------------------------+
+| Released under the terms & conditions of v2 of the GNU General Public|
+| License. For details refer to the included gpl.txt file or visit     |
+| http://gnu.org                                                       |
++----------------------------------------------------------------------+
+| $Id:: viewpage.php 1935 2008-10-29 23:42:42Z WanWizard              $|
++----------------------------------------------------------------------+
+| Last modified by $Author:: WanWizard                                $|
+| Revision number $Rev:: 1935                                         $|
++---------------------------------------------------------------------*/
 require_once dirname(__FILE__)."/../../includes/core_functions.php";
 require_once PATH_ROOT."/includes/theme_functions.php";
 
