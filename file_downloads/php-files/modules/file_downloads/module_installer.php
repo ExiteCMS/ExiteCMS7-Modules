9326c9840d854087129d507786b2ad8e15d42d84
root <root@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-11-13 21:06:26 +0000

    skeleton for a new common module: file downloads

---
+++ file_downloads/php-files/modules/file_downloads/module_installer.php
@@ -0,0 +1,148 @@
+<?php
+/*---------------------------------------------------------------------+
+| ExiteCMS Content Management System                                   |
++----------------------------------------------------------------------+
+| Copyright 2006-2008 Exite BV, The Netherlands                        |
+| for support, please visit http://www.exitecms.org                    |
++----------------------------------------------------------------------+
+| Some code derived from PHP-Fusion, copyright 2002 - 2006 Nick Jones  |
++----------------------------------------------------------------------+
+| Released under the terms & conditions of v2 of the GNU General Public|
+| License. For details refer to the included gpl.txt file or visit     |
+| http://gnu.org                                                       |
++----------------------------------------------------------------------+
+| $Id:: viewpage.php 1935 2008-10-29 23:42:42Z WanWizard              $|
++----------------------------------------------------------------------+
+| Last modified by $Author:: WanWizard                                $|
+| Revision number $Rev:: 1935                                         $|
++---------------------------------------------------------------------*/
+if (!checkrights("I") || !defined("iAUTH") || $aid != iAUTH || !defined('INIT_CMS_OK')) fallback(BASEDIR."index.php");
+
+/*---------------------------------------------------+
+| Module identification                              |
++----------------------------------------------------*/
+$mod_title = "File Downloads";							// title or name of this module
+$mod_description = "Module to provide a secure download page for files stored locally on the webserver";	// short description of it's purpose
+$mod_version = "1.0.0";									// module version number
+$mod_developer = "WanWizard";							// author's name
+$mod_email = "wanwizard@exitecms.org";
+$mod_weburl = "http://www.exitecms.org/";
+$mod_type = "M";
+
+/*---------------------------------------------------+
+| Module administration panel installation details   |
++----------------------------------------------------*/
+
+$mod_folder = "file_downloads";							// sub-folder of the /modules folder
+$mod_admin_image = "filedownloads.gif";					// icon to be used for the admin panel
+$mod_admin_panel = "admin.php";							// name of the admin panel for this module
+$mod_admin_rights = "wF";								// admin rights code. This HAS to be assigned by ExiteCMS to avoid duplicates!
+$mod_admin_page = 4;									// admin page this panel has to be placed on
+
+/*---------------------------------------------------+
+| Version and revision control                       |
++----------------------------------------------------*/
+
+// check for a minumum version of the ExiteCMS engine
+if (str_replace(".", "", $settings['version']) < 720) {
+	$mod_errors .= sprintf($locale['mod001'], '7.20');
+}
+// check for a maximum version of the ExiteCMS engine
+if (str_replace(".", "", $settings['version']) > 720) {
+	$mod_errors .= sprintf($locale['mod002'], '7.20');
+}
+// check for a specific revision number range that is supported
+if ($settings['revision'] < 0 || $settings['revision'] > 999999) {
+	$mod_errors .= sprintf($locale['mod003'], 0, 999999);
+}
+
+/*---------------------------------------------------+
+| Menu entries for this module                       |
++----------------------------------------------------*/
+
+$mod_site_links = array();								// site_links definitions. Multiple can be defined
+$mod_site_links[] = array('name' => $mod_title, 'url' => 'file_downloads.php', 'panel' => '', 'visibility' => 103);
+
+/*---------------------------------------------------+
+| locale strings for this module                     |
++----------------------------------------------------*/
+$localestrings = array();
+$localestrings['en'] = array();
+
+$localestrings['nl'] = array();
+
+/*---------------------------------------------------+
+| commands to execute when installing this module    |
++----------------------------------------------------*/
+
+$mod_install_cmds = array();							// commands to execute when installing this module
+
+// adverts: advertising table
+$mod_install_cmds[] = array('type' => 'db', 'value' => "CREATE TABLE ##PREFIX##file_downloads (
+  fd_id smallint(5) NOT NULL auto_increment,
+  fd_path varchar(255) NOT NULL default '',
+  fd_group smallint(5) NOT NULL default '103',
+  PRIMARY KEY  (fd_id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+// add a user group for this module
+$mod_install_cmds[] = array('type' => 'db', 'value' => "INSERT INTO ##PREFIX##user_groups (group_ident, group_name, group_description, group_forumname, group_visible) VALUES ('".$mod_admin_rights."01', 'File Downloads', 'File Downloads', '', '1')");
+
+$mod_install_cmds[] = array('type' => 'function', 'value' => "install_function");
+
+/*---------------------------------------------------+
+| commands to execute when uninstalling this module  |
++----------------------------------------------------*/
+
+$mod_uninstall_cmds = array();							// commands to execute when uninstalling this module
+
+$mod_uninstall_cmds[] = array('type' => 'db', 'value' => "DROP TABLE ##PREFIX##file_downloads");
+
+$mod_uninstall_cmds[] = array('type' => 'function', 'value' => "uninstall_function");
+
+/*---------------------------------------------------+
+| function for special installations                 |
++----------------------------------------------------*/
+if (!function_exists('install_function')) {
+	function install_function() {
+
+		global $db_prefix, $locale, $mod_title, $mod_admin_rights;
+
+		// update the visiblity of the menu link
+
+		// get the group_id of the File Downloads group
+		$group = dbarray(dbquery("SELECT group_id FROM ".$db_prefix."user_groups WHERE group_ident = '".$mod_admin_rights."01'"));
+		if (is_array($group)) {
+			// modify the visibility of the URL
+			$result = dbquery("UPDATE ".$db_prefix."site_links SET link_visibility = '".$group['group_id']."' WHERE link_name = '".$mod_title."' AND link_url = 'file_downloads.php'");
+		}
+	}
+}
+/*---------------------------------------------------+
+| function for special de-installations              |
++----------------------------------------------------*/
+if (!function_exists('uninstall_function')) {
+	function uninstall_function() {
+	}
+}
+
+/*---------------------------------------------------+
+| function to upgrade from a previous revision       |
++----------------------------------------------------*/
+if (!function_exists('module_upgrade')) {
+	function module_upgrade($current_version) {
+
+		global $db_prefix, $locale;
+
+		switch($current_version) {
+			case "0.0.1":
+				// pre-release version, no database or other changes
+			case "1.0.0":
+				// current version
+			default:
+				// do this at every upgrade
+		}
+
+	}
+}
+?>
