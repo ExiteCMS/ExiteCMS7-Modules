6e3a8191fce151351275018c4c356cdd4717a902
webmaster <webmaster@f05d0674-473a-0410-9a19-83e10c34e0e8>
2008-12-08 13:58:56 +0000

    moved shoutbox admin template from the generic templates directory to the module directory updated the shoutbox admin code to use the generic parsemessage() function

--- shoutbox_panel/php-files/modules/shoutbox_panel/shoutbox_archive.php
+++ shoutbox_panel/php-files/modules/shoutbox_panel/shoutbox_archive.php
@@ -16,43 +16,91 @@
 | Last modified by $Author::                                          $|
 | Revision number $Rev::                                              $|
 +---------------------------------------------------------------------*/
-require_once dirname(__FILE__)."../../../includes/core_functions.php";
+require_once dirname(__FILE__)."/../../includes/core_functions.php";
 require_once PATH_ROOT."/includes/theme_functions.php";
 
 // load message parsing functions
 require_once PATH_INCLUDES."forum_functions_include.php";
 
+// load the locale for this module
+locale_load("modules.shoutbox_panel");
+
 // temp storage for template variables
 $variables = array();
 
-// check howmany shouts we have
-$variables['rows'] = dbfunction("COUNT(shout_id)","shoutbox");
+// check for the proper admin access rights
+if (!checkrights("S") || !defined("iAUTH") || $aid != iAUTH) fallback(BASEDIR."index.php");
 
-// make sure rowstart has a valid value
-if (!isset($rowstart) || !isNum($rowstart)) $rowstart = 0;
-$variables['rowstart'] = $rowstart;
+// make sure the parameter is valid
+if (isset($shout_id) && !isNum($shout_id)) fallback(FUSION_SELF.$aidlink);
+if (!isset($action)) $action = "";
 
-// shouts present? Get them!
-$variables['shouts'] = array();
-if ($variables['rows'] != 0) {
+if (isset($status)) {
+	if ($status == "delall") {
+		$title = $locale['400'];
+		$variables['message'] = $numr." ".$locale['401'];
+	} elseif ($status == "del") {
+		$title = $locale['404'];
+		$variables['message'] = $locale['405'];
+	}
+	// define the message panel variables
+	$variables['bold'] = true;
+	$template_panels[] = array('type' => 'body', 'name' => 'admin.shoutbox.status', 'title' => $title, 'template' => '_message_table_panel.tpl');
+	$template_variables['admin.shoutbox.status'] = $variables;
+	$variables = array();
+}
+
+if ($action == "deleteshouts") {
+	$deletetime = time() - ($_POST['num_days'] * 86400);
+	$result = dbquery("SELECT * FROM ".$db_prefix."shoutbox WHERE shout_datestamp < '$deletetime'");
+	$numrows = dbrows($result);
+	$result = dbquery("DELETE FROM ".$db_prefix."shoutbox WHERE shout_datestamp < '$deletetime'");
+	redirect(FUSION_SELF.$aidlink."&status=delall&numr=$numrows");
+} else if ($action == "delete") {
+	$result = dbquery("DELETE FROM ".$db_prefix."shoutbox WHERE shout_id='$shout_id'");
+	redirect(FUSION_SELF.$aidlink."&status=del");
+} else {
+	if (isset($_POST['saveshout'])) {
+		if ($action == "edit") {
+			$shout_message = str_replace("\n", " ", $_POST['shout_message']);
+			$shout_message = preg_replace("/^(.{255}).*$/", "$1", $shout_message);
+			$shout_message = preg_replace("/([^\s]{25})/", "$1\n", $shout_message);
+			$shout_message = stripinput($shout_message);
+			$shout_message = str_replace("\n", "<br>", $shout_message);
+			$result = dbquery("UPDATE ".$db_prefix."shoutbox SET shout_message='$shout_message' WHERE shout_id='$shout_id'");
+			redirect(FUSION_SELF.$aidlink);
+		}
+	}
+	$variables['action'] = $action;
+	if ($action == "edit") {
+		$result = dbquery("SELECT * FROM ".$db_prefix."shoutbox WHERE shout_id='$shout_id'");
+		$data = dbarray($result);
+		$data['shout_message'] = str_replace("<br>", "", $data['shout_message']);
+		$variables['editdata'] = $data;
+	}
+
+	$result = dbquery("SELECT * FROM ".$db_prefix."shoutbox");
+	$rows = dbrows($result);
+	$variables['rows'] = $rows;
+	if (!isset($rowstart) || !isNum($rowstart)) $rowstart = 0;
+	$variables['rowstart'] = $rowstart;
+	$variables['pagenavurl'] = FUSION_SELF.$aidlink."&amp;";
+	
 	$result = dbquery(
 		"SELECT * FROM ".$db_prefix."shoutbox LEFT JOIN ".$db_prefix."users
 		ON ".$db_prefix."shoutbox.shout_name=".$db_prefix."users.user_id
-		ORDER BY shout_datestamp DESC LIMIT $rowstart,".$settings['numofthreads']
-	);
+		ORDER BY shout_datestamp DESC LIMIT $rowstart,".$settings['numofthreads']);
+
+	$variables['shouts'] = array();
 	while ($data = dbarray($result)) {
 		$data['shout_message'] = parsemessage(array(), $data['shout_message'], true, true);
-		// store the data
-		$variables['shouts'][]  = $data;
+		$variables['shouts'][] = $data;
 	}
 }
 
-// check if the current user is a shoutbox admin
-$variables['is_admin'] = iMEMBER && checkrights("S");
-
-// define the search body panel variables
-$template_panels[] = array('type' => 'body', 'name' => 'shoutbox_archive', 'template' => 'modules.shoutbox_panel.archive.tpl');
-$template_variables['shoutbox_archive'] = $variables;
+// define the admin body panel
+$template_panels[] = array('type' => 'body', 'name' => 'admin.shoutbox', 'template' => 'modules.shoutbox_panel.admin.tpl', 'locale' => "modules.shoutbox_panel");
+$template_variables['admin.shoutbox'] = $variables;
 
 // Call the theme code to generate the output for this webpage
 require_once PATH_THEME."/theme.php";
